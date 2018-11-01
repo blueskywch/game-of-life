636739a72752871281d0cf6cdaefe9e7c4ef58d9
3218143 <bluesky_wch@163.com>
2018-11-01 15:54:02 +0800

    t

--- gameoflife-acceptance-tests/src/test/java/com/wakaleo/gameoflife/webtests/pages/ShowGridPage.java
+++ gameoflife-acceptance-tests/src/test/java/com/wakaleo/gameoflife/webtests/pages/ShowGridPage.java
@@ -12,7 +12,6 @@ public class ShowGridPage extends GameOfLifePage {
 
     @FindBy(id = "submit")
     WebElement nextButton;
-    fdsf
 
     public ShowGridPage(WebDriver driver) {
         super(driver);
